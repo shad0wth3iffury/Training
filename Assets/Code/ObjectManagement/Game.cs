2f0727b7781ee2b83a95e18b4225664d3553af83
shad0wth3iffury <shad0wth3iffury@hotmail.com>
2018-09-05 09:17:11 -0700

    Spawning and saving

---
+++ Assets/Code/ObjectManagement/Game.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public Transform prefab;
+
+    public KeyCode createKey = KeyCode.C;
+    public KeyCode newGameKey = KeyCode.N;
+    public KeyCode saveGameKey = KeyCode.S;
+
+    List<Transform> objects;
+    string savePath;
+
+    private void Awake()
+    {
+        savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+    }
+
+    private void Start()
+    {
+        objects = new List<Transform>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(createKey))
+        {
+            CreateObject();
+        }
+        else if (Input.GetKeyDown(newGameKey))
+        {
+            BeginNewGame();
+        }
+        else if (Input.GetKeyDown(saveGameKey))
+        {
+            Save();
+        }
+    }
+
+    void CreateObject()
+    {
+        Transform t = Instantiate(prefab);
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+        objects.Add(t);
+    }
+
+    void BeginNewGame()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            Destroy(objects[i].gameObject);
+        }
+        objects.Clear();
+    }
+
+    void Save()
+    {
+        using (var writer = new BinaryWriter(File.Open(savePath, FileMode.Create)))
+        {
+            Debug.Log(savePath);
+            writer.Write(objects.Count);
+        }
+    }
+}
